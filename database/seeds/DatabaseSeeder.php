ede63a3b2bc19fa4a29fcda5044fc89e4180ec26
Hong Son <hson8711@gmail.com>
2019-12-15 01:48:24 +0700

    update_10

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ProductTableSeeder::class);
+//        $this->call(ProductTableSeeder::class);
         $this->call(VehiclesTableSeeder::class);
         $this->call(ModelCarSeeder::class);
         $this->call(UserTableSeeder::class);
