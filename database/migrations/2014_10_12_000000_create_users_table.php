7b4cc8469c5d8a9b3fa73eecdb04f15b9226239d
hieuulv <hieuu.dev99@gmail.com>
2019-12-18 19:08:51 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email',100)->unique();
             $table->string('identity_card', 20)->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone', 20)->nullable();
+            $table->string('phone', 20)->nullable()->unique();
             $table->text('address')->nullable();
             $table->string('image')->default('default_avata.png');
             $table->string('password');
