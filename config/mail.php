6f169345d5ca6d7fc4b68b7a6fad317b8f0d6b5a
hieuulv <hieuu.dev99@gmail.com>
2020-01-13 23:22:01 +0700

    update

--- config/mail.php
+++ config/mail.php
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME', 'hieuu.dev99@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD', 'fzzgehomikdiduhk'),
 
     /*
     |--------------------------------------------------------------------------
